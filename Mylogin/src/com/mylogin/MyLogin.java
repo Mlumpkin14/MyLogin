aaafd3b652d6d01498b2753e2a3f2e5bbc0039c4
Matthew <mattlumpkin04@gmail.com>
2020-02-05 11:28:35 -0500

    First commit for loginPage

---
+++ Mylogin/src/com/mylogin/MyLogin.java
@@ -0,0 +1,49 @@
+package com.mylogin;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyLogin
+ */
+@WebServlet("/MyLogin")
+public class MyLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyLogin() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("Password");
+		
+		if(username.equals("admin") && password.equals("admin")) {
+			response.sendRedirect("submit.html");
+		} else {
+			response.sendRedirect("error.html");
+		}
+	
+		//doGet(request, response);
+	}
+
+}
